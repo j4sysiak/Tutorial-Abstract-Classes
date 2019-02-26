2ec7f3981bdd345be4ba06f890e048d6854e2410
j4sysiak <j4sysiak@gmail.com>
2019-02-26 06:17:51 +0100

    2

--- Tutorial-Abstract-Classes/src/App.java
+++ Tutorial-Abstract-Classes/src/App.java
@@ -2,6 +2,12 @@
 public class App {
 	 
 	public static void main(String[] args) {
+		
+		Camera camera1 = new Camera();
+		camera1.setId(1);
+		
+		Car car1 = new Car();
+		car1.setId(2);
 		 
 	}
 }
--- Tutorial-Abstract-Classes/src/Machine.java
+++ Tutorial-Abstract-Classes/src/Machine.java
@@ -13,6 +13,5 @@ public class Machine {
 		this.id = id;
 	}
 
- 
-
+  
 }
